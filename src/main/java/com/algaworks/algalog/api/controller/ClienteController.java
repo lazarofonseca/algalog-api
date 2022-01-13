7e8676fb197fe8e4ff64e14f777738b435bfc110
lazarofonseca <lazaronobrega3@gmail.com>
2022-01-13 07:10:13 -0300

    Iniciando com JpaRepository

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		Cliente cliente1 = new Cliente(1L, "João", "joaosilva@gmail.com", "83 6458-8582");
-		Cliente cliente2 = new Cliente(2L, "Maria", "mariajose@hotmail.com", "83 3375-4145");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
