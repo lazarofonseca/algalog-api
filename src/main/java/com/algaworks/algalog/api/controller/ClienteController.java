86c191914fb66bd2fe5dc4e194b4f9fb59578898
lazarofonseca <lazaronobrega3@gmail.com>
2022-01-13 07:28:35 -0300

    Criando Metódos personalizados

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,24 +2,28 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	//UMA MANEIRA DE FAZER ACESSO AO BANCO COM ENTITYMANAGER PARA PROJETOS PEQUENO TESTES E ESTUDOS
+	//@PersistenceContext
+	//private EntityManager manager;
+	@Autowired
+	private ClienteRepository ClienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return ClienteRepository.findByNome("Lázaro");
+		
+		//return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
